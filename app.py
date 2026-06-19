c17fda6ceabb3ff49eb84bcae691f5a513aa5a69
Dotriix <74355747+Dotriix@users.noreply.github.com>
2026-06-19 11:44:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def scan_url():
             details = f"Le système a détecté un contenu suspect : {cl['raison']}"
             
     else:
-        verdict = "            ✅ LIEN SÉCURISÉ ✅"
+        verdict = "              ✅ LIEN SÉCURISÉ ✅"
         details = "\n\nLe système de protection O.R.I.O.N n'a détecté aucune anomalie provenant de ce lien."
         
     return jsonify({"verdict": verdict, "details": details})
