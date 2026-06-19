3dc1764e911c276c401aaf107a57a1dcecd9c04f
Dotriix <74355747+Dotriix@users.noreply.github.com>
2026-06-19 09:08:59 +0400

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def scan_url():
         verdict = "                   ⚠️ MENACE ⚠️"
         details = f"\n\nLe système O.R.I.O.N a détecté : {vt_result['detec']} alerte(s). {cl_result['raison']}"
     else:
-        verdict = "\n\n                  ✅ SÉCURISÉ ✅"
+        verdict = "                  ✅ SÉCURISÉ ✅"
         details = "\n\nAucun élément suspect n'a été détecté."
 
     return jsonify({
